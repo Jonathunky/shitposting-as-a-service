04589e3742fa7be0cbdfc7f0f0ff74af8ee804c1
Jonathunky <12529409+Jonathunky@users.noreply.github.com>
2023-11-07 17:58:14 +0100

    Smol changes

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  //{ href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'My Projects' },
+  { href: '/about', title: 'About Me' },
 ]
 
 export default headerNavLinks
